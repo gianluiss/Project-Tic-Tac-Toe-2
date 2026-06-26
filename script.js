fe3e00331dd59ab65d5ce7f0b97e8722febd7c89
gianluiss <gianluistapia28@gmail.com>
2026-06-26 13:48:55 +0800

    Added initial gameController()

--- script.js
+++ script.js
@@ -24,16 +24,16 @@ const gameBoard = (() => {
     }
 
     // Make function to place a marker
-    const placeMarker = (row, col, player) => {
-        board[row][col].markCell(player);
+    const placeMarker = (row, col, playerMarker) => {
+        board[row][col].markCell(playerMarker);
     }
 
     return {getBoard, displayBoard, placeMarker};
 })();
 
-gameBoard.placeMarker(2,1, 1);
+//gameBoard.placeMarker(2,1, 1);
 //console.log(gameBoard.getBoard());
-gameBoard.displayBoard();
+//gameBoard.displayBoard();
 
 /*
 for(let i = 0; i < 5; i++) {
@@ -61,4 +61,59 @@ function createCell() {
 const cell = createCell();
 
 console.log(cell.getMarker());
+*/
+
+/*
+ ** The gameController will be responsible for controlling the
+ ** flow and state of the game's turns, as well as whether
+ ** anybody has won the game
+*/
+
+const gameController = (() => {
+
+    const board = gameBoard;
+
+    const players = [
+        {
+            name: "Player One",
+            marker: 1, // X
+        },
+        {
+            name: "Player Two",
+            marker: 2, // O
+        }
+    ]
+
+    let activePlayer = players[0];
+
+    const switchPlayerTurn = () => { 
+        players[0] === activePlayer ? activePlayer = players[1] : activePlayer = players[0];
+    }
+
+    const getActivePlayer = () => activePlayer;
+
+    const playNewRound = () => {
+        board.displayBoard();
+        console.log(`Now it is ${activePlayer}'s turn...`);
+    }
+
+    // Will put a (row, column, player) parameter in the future, for now prompt
+    const playRound = () => {
+        let row = prompt("Enter Row");
+        let col = prompt("Enter Column");
+
+        // Make a checker if current position is occupied
+
+        board.placeMarker(row, col, activePlayer.marker);
+
+        // Check if player won
+    }
+
+    return {switchPlayerTurn, getActivePlayer};
+})();
+
+/*
+console.log(gameController.getActivePlayer());
+gameController.switchPlayerTurn();
+console.log(gameController.getActivePlayer());
 */
