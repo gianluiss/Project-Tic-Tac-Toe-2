9a1ed397c35cfd698c02d8847eec900cb35af7b5
gianluiss <gianluistapia28@gmail.com>
2026-07-02 01:26:26 +0800

    Added initial game flow(rounds) & initial win con

--- script.js
+++ script.js
@@ -13,13 +13,27 @@ const gameBoard = (() => {
 
     const getBoard = () => board;
 
+    /* //For node outputs
     const displayBoard = () => {
         for(let i = 0; i < size; i++) {
             for(let j = 0; j < size; j++) {
-                process.stdout.write( String(board[i][j].getMarker()) );
+                process.stdout.write( String(board[i][j].getMarker()) ); //uncomment if want to see console output
                 //console.log(board[i][j].getMarker());
             }
             console.log();
+        }
+    }
+    */
+
+    const displayBoard = () => {
+        /*
+        console.log(`${board[0][0]} | ${board[0][1]} | ${board[0][2]}`);
+        console.log(`${board[1][0]} | ${board[1][1]} | ${board[1][2]}`);
+        console.log(`${board[2][0]} | ${board[2][1]} | ${board[2][2]}`);
+        */
+
+        for(let i = 0; i < 3; i++) {
+            console.log(`${board[i][0].getMarker()} | ${board[i][1].getMarker()} | ${board[i][2].getMarker()}`);
         }
     }
 
@@ -28,7 +42,30 @@ const gameBoard = (() => {
         board[row][col].markCell(playerMarker);
     }
 
-    return {getBoard, displayBoard, placeMarker};
+    // Check current position if occupied
+    const isOccupied = (row, col) => (board[row][col] !== 0);
+
+    // Win condition (Check if player won)
+    const isWonStraight = (playerMarker) => {
+
+        // Check horizontals
+        let isWon = true;
+
+        for(let row = 0; row < board.size(); row++) {
+
+            if(!isWon) break;
+
+            for(let col = 0; col < board.size(); col++) {
+                if(board[row][col] !== playerMarker) {
+                    isWon = false;
+                }
+            }
+        }
+
+        return isWon;
+    }
+
+    return {getBoard, displayBoard, placeMarker, isOccupied, isWon};
 })();
 
 //gameBoard.placeMarker(2,1, 1);
@@ -93,23 +130,38 @@ const gameController = (() => {
     const getActivePlayer = () => activePlayer;
 
     const playNewRound = () => {
-        board.displayBoard();
-        console.log(`Now it is ${activePlayer}'s turn...`);
+        board.displayBoard(); //Console debugging
+        console.log(`Now it is ${activePlayer.name}'s turn...`);
     }
 
     // Will put a (row, column, player) parameter in the future, for now prompt
     const playRound = () => {
-        let row = prompt("Enter Row");
-        let col = prompt("Enter Column");
 
-        // Make a checker if current position is occupied
+        const actualBoard = board.getBoard();
+
+        let row = Number(prompt("Enter Row"));
+        let col = Number(prompt("Enter Column"));
+
+        while(actualBoard[row][col].getMarker() !== 0) {
+            console.log((actualBoard[row][col].getMarker()));
+            alert(`Please try again... Row: ${row} | Col: ${col} is occupied.`);
+            row = Number(prompt("Enter Row"));
+            col = Number(prompt("Enter Column"));
+        }
 
         board.placeMarker(row, col, activePlayer.marker);
 
+        // display current mark for debugging
+        console.log(actualBoard[row][col].getMarker());        
+
+
+        //board.displayBoard(); //console debugging
+
         // Check if player won
+
     }
 
-    return {switchPlayerTurn, getActivePlayer};
+    return {switchPlayerTurn, getActivePlayer, playNewRound, playRound};
 })();
 
 /*
@@ -117,3 +169,13 @@ console.log(gameController.getActivePlayer());
 gameController.switchPlayerTurn();
 console.log(gameController.getActivePlayer());
 */
+
+while(true) {
+    gameController.playNewRound();
+    gameController.playRound();
+    gameController.switchPlayerTurn();
+
+    if(prompt("Continue [0] to exit") === '0') {
+        break;
+    };
+}
